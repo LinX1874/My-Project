56dcee1601b84df168de043af976d01a0b50e906
tangly <tangly@bestsep.com>
2018-05-15 09:30:39 +0800

    目录调整

--- common-parent/src/main/java/com/tangly/CommonParentApplication.java
+++ common-parent/src/main/java/com/tangly/CommonParentApplication.java
@@ -1,17 +1,13 @@
 package com.tangly;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 /**
  * date: 2018/5/10 12:21 <br/>
  *
  * @author tangly
  * @since JDK 1.7
  */
-@SpringBootApplication
 public class CommonParentApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CommonParentApplication.class, args);
+        System.out.println("这只是个父类");
     }
 }
